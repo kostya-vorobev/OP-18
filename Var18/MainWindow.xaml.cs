6a692dfb61c9cf0d85db891bc057bae70da8415b
kostya-vorobev <105592308+kostya-vorobev@users.noreply.github.com>
2025-03-28 00:47:12 +0700

    Добавьте файлы проекта.

---
+++ Var18/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Var18
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+        private void ToggleComboBoxDropDown(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button &&
+                button.TemplatedParent is ComboBox comboBox)
+            {
+                comboBox.IsDropDownOpen = !comboBox.IsDropDownOpen;
+            }
+        }
+    }
+
+
+}
